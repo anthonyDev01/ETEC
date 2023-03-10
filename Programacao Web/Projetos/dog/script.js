3871fec29138aa3f549df06f942b051b54614227
ANTHONY <anthonychukwudiDev@outlook.com>
2023-03-10 00:48:24 -0300

    Projeto realizado no primeiro ano de programacao web

---
+++ Programacao Web/Projetos/dog/script.js
@@ -0,0 +1,45 @@
+var button = document.getElementById("botao")
+
+button.addEventListener("click", function click(e){
+
+    let dog = document.getElementById("dogRight");  
+    let posInicial = 0;
+    let posFinal = 90;
+    let intervalFrente = setInterval(andar, 50);
+
+      function andar() {
+        if (posInicial == posFinal) {       
+          if (dog.src.match("dog")) {
+            dog.src = "img/dogleft.gif";
+            clearInterval(intervalFrente);
+            voltar();
+          }
+        }
+        else {
+          posInicial++;
+          dog.style.left = posInicial + "%";   
+        }
+      }
+    
+      function voltar() {
+        posInicial = 90;
+        posFinal = 0;
+        let intervalTras = setInterval(andarParaTras, 50);
+    
+        function andarParaTras() {
+          if (posInicial == posFinal) {
+            if (dog.src.match("dog")) {
+              dog.src = "img/dogright.gif";
+    
+              clearInterval(intervalTras);
+              mover();
+            }
+          }
+          else { 
+            posInicial--
+            dog.style.left = posInicial + "%";
+          }
+        }
+      }     
+  button.removeEventListener("click", click);
+})
